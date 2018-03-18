4f68bad033e68e875084acc94d57b18e4b6eae76
patrickvl <pvanlogchem@gmail.com>
2018-03-18 16:30:41 +0100

    Rebase fixes

--- src/CxbxKrnl/EmuD3D8.cpp
+++ src/CxbxKrnl/EmuD3D8.cpp
@@ -7306,7 +7306,7 @@ VOID WINAPI XTL::EMUPATCH(D3DDevice_DrawVerticesUP)
 		DrawContext.dwVertexCount = VertexCount;
 		DrawContext.pXboxVertexStreamZeroData = pVertexStreamZeroData;
 		DrawContext.uiXboxVertexStreamZeroStride = VertexStreamZeroStride;
-		DrawContext.hVertexShader = g_CurrentVertexShader;
+		DrawContext.hVertexShader = g_CurrentXboxVertexShaderHandle;
 
 		CxbxDrawPrimitiveUP(DrawContext);
     }
