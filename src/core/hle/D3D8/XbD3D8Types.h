b6388ab8da8e90d271c1b8db2c6bf37cda9bcc6e
PatrickvL <pvanlogchem@gmail.com>
2019-09-05 11:46:02 +0200

    Cleanup Xbox symbol declarations (moving Cxbx specific types aside). Also check vertex shader header.

--- src/core/hle/D3D8/XbD3D8Types.h
+++ src/core/hle/D3D8/XbD3D8Types.h
@@ -1044,8 +1044,6 @@ typedef struct _X_VERTEXSHADERINPUT
 }
 X_VERTEXSHADERINPUT;
 
-const int X_VSH_NBR_ATTRIBUTES = 16;
-
 // ******************************************************************
 // * X_VERTEXATTRIBUTEFORMAT
 // ******************************************************************
