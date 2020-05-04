7c1c40b46b29bddcd15cce36b8604d44ca748d87
patrickvl <pvanlogchem@gmail.com>
2020-05-04 22:00:11 +0200

    Define X_D3DVertexShader.Flags values

--- src/core/hle/D3D8/XbD3D8Types.h
+++ src/core/hle/D3D8/XbD3D8Types.h
@@ -1101,6 +1101,16 @@ struct X_D3DVertexShader
 	DWORD FunctionData[X_VSH_MAX_INSTRUCTION_COUNT]; // probably the binary function data and ?constants? (data continues futher outside this struct, up to TotalSize DWORD's)
 };
 
+// X_D3DVertexShader.Flags values :
+#define X_VERTEXSHADER_FLAG_WRITE           (1 <<  0) // = 0x0001
+#define X_VERTEXSHADER_FLAG_PASSTHROUGH     (1 <<  1) // = 0x0002
+#define X_VERTEXSHADER_FLAG_STATE           (1 <<  3) // = 0x0008
+#define X_VERTEXSHADER_FLAG_PROGRAM         (1 <<  4) // = 0x0010
+#define X_VERTEXSHADER_FLAG_HASDIFFUSE      (1 << 10) // = 0x0400
+#define X_VERTEXSHADER_FLAG_HASSPECULAR     (1 << 11) // = 0x0800
+#define X_VERTEXSHADER_FLAG_HASBACKDIFFUSE  (1 << 12) // = 0x1000
+#define X_VERTEXSHADER_FLAG_HASBACKSPECULAR (1 << 13) // = 0x2000
+
 // vertex shader input registers for fixed function vertex shader
 
 //          Name                   Register number      D3DFVF
